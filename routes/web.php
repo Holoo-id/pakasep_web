5d62e4a302dcf00a1c9e73d24f2ffc523e6494b9
Andika. R <andika15ramdani@gmail.com>
2021-03-25 11:39:36 +0700

    save today

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\RumahController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -12,14 +14,15 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::get('/', 'App\Http\Controllers\RumahController@index')->name('dashboard');
+// Route::get('/', 'App\Http\Controllers\RumahController@index')->name('dashboard');
+Route::get('/', [RumahController::class, 'index'])->name('dashboard');
 
-Route::get('/post', 'App\Http\Controllers\RumahController@post');
+// Route::get('/post', 'App\Http\Controllers\RumahController@post');
 
-Route::post('/admin/tambah', 'App\Http\Controllers\RumahController@store');
-Route::get('/hapus/{id}', 'App\Http\Controllers\RumahController@hapus');
-Route::post('/edit/{id}', 'App\Http\Controllers\RumahController@edit');
-Route::get('/admin/dataRumah', 'App\Http\Controllers\RumahController@dataRumah')->name('select');
-Route::get('/admin/dataRumah/pdfrumah', 'App\Http\Controllers\RumahController@cetak_pdf');
-Route::get('/admin/pengajuan', 'App\Http\Controllers\PengajuanRumahController@index');
-Route::get('/admin/pengajuan/pdfpengajuan', 'App\Http\Controllers\PengajuanRumahController@cetak_pdf');
+// Route::post('/admin/tambah', 'App\Http\Controllers\RumahController@store');
+// Route::get('/hapus/{id}', 'App\Http\Controllers\RumahController@hapus');
+// Route::post('/edit/{id}', 'App\Http\Controllers\RumahController@edit');
+// Route::get('/admin/dataRumah', 'App\Http\Controllers\RumahController@dataRumah')->name('select');
+// Route::get('/admin/dataRumah/pdfrumah', 'App\Http\Controllers\RumahController@cetak_pdf');
+// Route::get('/admin/pengajuan', 'App\Http\Controllers\PengajuanRumahController@index');
+// Route::get('/admin/pengajuan/pdfpengajuan', 'App\Http\Controllers\PengajuanRumahController@cetak_pdf');
